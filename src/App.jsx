87363a06ba5fdfae4d2d9053f7afe7e72c8faade
Dell <yashunhawane@gmail.com>
2024-08-23 14:21:24 +0530

    remove login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 import BookingList from "./component/BookingList";
-import LoginPage from "./component/LoginPage";
-import PrivateRoute from "./component/PrivateRoute";
+// import LoginPage from "./component/LoginPage";
+// import PrivateRoute from "./component/PrivateRoute";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route
-        path="/bookings"
-        element={
-          <PrivateRoute>
-            <BookingList />
-          </PrivateRoute>
-        }
-      />
-    </Routes>
+    <BookingList />
+
 
   );
 };
 
 export default App;
+
+
+{/* <Routes>
+<Route path="/" element={<LoginPage />} />
+<Route
+  path="/bookings"
+  element={
+    <PrivateRoute>
+      <BookingList />
+    </PrivateRoute>
+  }
+/>
+</Routes> */}
